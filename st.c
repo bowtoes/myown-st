418a937170a9873365510f146e56b834114eb5a7
Ivan Tham <pickfire@riseup.net>
2020-04-19 19:38:39 +0200

    Update XIM cursor position only if changed
    Updating XIM cursor position is expensive, so only update it when cursor
    position changed.

--- st.c
+++ st.c
@@ -2723,6 +2723,7 @@ void
 drawregion(int x1, int y1, int x2, int y2)
 {
 	int y;
+
 	for (y = y1; y < y2; y++) {
 		if (!term.dirty[y])
 			continue;
@@ -2735,7 +2736,7 @@ drawregion(int x1, int y1, int x2, int y2)
 void
 draw(void)
 {
-	int cx = term.c.x;
+	int cx = term.c.x, ocx = term.ocx, ocy = term.ocy;
 
 	if (!xstartdraw())
 		return;
@@ -2752,9 +2753,11 @@ draw(void)
 	if (term.scr == 0) /* Scrollback */
 		xdrawcursor(cx, term.c.y, term.line[term.c.y][cx],
 				term.ocx, term.ocy, term.line[term.ocy][term.ocx]);
-	term.ocx = cx, term.ocy = term.c.y;
+	term.ocx = cx;
+	term.ocy = term.c.y;
 	xfinishdraw();
-	xximspot(term.ocx, term.ocy);
+	if (ocx != term.ocx || ocy != term.ocy)
+		xximspot(term.ocx, term.ocy);
 }
 
 void
